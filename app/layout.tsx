51d128087639fc18cbe1a7a665d4624e1687b0f2
wemovenewyork <wemovenewyork.net@gmail.com>
2026-04-01 17:40:48 -0500

    Switch font to Poppins, increase base size to 19px

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link href="https://fonts.cdnfonts.com/css/resolve" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </head>
       <body>
         <a href="#main-content" className="skip-link">Skip to main content</a>
